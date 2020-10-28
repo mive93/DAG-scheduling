0759c99097a786806c497b4343c6959e9aad3e84
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-10-28 17:43:59 +0100

    Adding missed files
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

---
+++ include/task.h
@@ -0,0 +1,45 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <vector>
+#include <map>
+
+#include <yaml-cpp/yaml.h>
+
+class SubTask{
+
+    public:
+
+    int id = 0;
+    int c = 0; // WCET
+    bool isCond = false; 
+    std::vector<SubTask*> succ;
+    std::vector<SubTask*> prec;
+
+    int accWork = 0; // accumulated workload
+
+    
+};
+
+class DAGTask{
+
+    int t = 0;  //period
+    int d = 0;  //deadline
+    std::vector<SubTask> V;
+
+    int L = 0; // longest chain
+    int vol  = 0; //volume
+
+
+    public:
+    DAGTask(){};
+    DAGTask(const int T, const int D): t(T), d(D) {};
+
+    ~DAGTask(){};
+    void readTaskFromYaml(const std::string& params_path);
+
+    friend std::ostream& operator<<(std::ostream& os, const DAGTask& t);
+
+};
+
+#endif /* TASK_h */
