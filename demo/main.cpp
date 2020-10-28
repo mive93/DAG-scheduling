107b535e827b594fd2c641b7d834c7f6599088be
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-10-28 17:41:44 +0100

    basic DAG task
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "task.h"
 
 int main(int argc, char **argv){
 
     std::cout<<"This is a DAG!"<<std::endl;
+
+    DAGTask t;
+    t.readTaskFromYaml("../demo/taskset.yaml");
+
+    std::cout<<t<<std::endl;
 
 }
--- src/task.cpp
+++ src/task.cpp
@@ -1,9 +1,62 @@
+#include "task.h"
 
-class Task{
+#include <iostream>
 
-    int t;
-    int d;
+std::ostream& operator<<(std::ostream& os, const DAGTask& t)
+{
 
-    public:
-    Task(const int T, const int D): t(T), d(D) {};
-};
+    os<<"----------------------------------------------------\n";
+    os<< "deadline :" << t.d <<std::endl;
+    os<< "period :" << t.t <<std::endl;
+    os<< "vertices :"<<std::endl;
+    for(auto v: t.V){
+        os<< "\t v_" << v.id << " - c: "<<v.c <<" \tsucc: ";
+        for(auto s:v.succ)
+            os<< s->id << " ";
+        os<<" \tprec: ";
+        for(auto p:v.prec)
+            os<< p->id << " ";
+        os<<" \n";
+    }
+    os<<"----------------------------------------------------\n";
+    return os;
+
+}
+
+void DAGTask::readTaskFromYaml(const std::string& params_path){
+    YAML::Node config   = YAML::LoadFile(params_path);
+    YAML::Node tasks = config["tasks"];
+
+    for(int i=0; i<tasks.size(); i++){
+        t = tasks[i]["t"].as<int>();
+        d = tasks[i]["d"].as<int>();
+
+        YAML::Node vert = tasks[i]["vetices"];
+
+        std::map<int, int> id_pos;
+
+        for(int j=0; j<vert.size(); j++){
+            SubTask v;
+            v.id = vert[j]["id"].as<int>();
+            v.c = vert[j]["c"].as<int>();
+
+            id_pos[v.id] = j;
+
+            V.push_back(v);
+        }
+
+        YAML::Node edges = tasks[i]["edges"];
+        int form_id, to_id;
+        for(int j=0; j<edges.size(); j++){
+
+            //add check
+            form_id = id_pos[edges[j]["from"].as<int>()];
+            to_id = id_pos[edges[j]["to"].as<int>()];
+
+            V[form_id].succ.push_back(&V[to_id]);
+            V[to_id].prec.push_back(&V[form_id]);
+        }
+    }
+}
+
+
