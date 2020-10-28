a2b93bfbe472d06f97ff9b4db9bc546f45176309
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-10-28 16:17:00 +0100

    Hello world
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

---
+++ demo/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+
+    std::cout<<"This is a DAG!"<<std::endl;
+
+}
---
+++ src/task.cpp
@@ -0,0 +1,9 @@
+
+class Task{
+
+    int t;
+    int d;
+
+    public:
+    Task(const int T, const int D): t(T), d(D) {};
+};
